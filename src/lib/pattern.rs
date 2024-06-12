36444d365bf9d3b6b1e7f603c396f1c0fa2bb85d
Martin Olsen <Martin.Olsen2@protonmail.com>
2024-06-12 13:11:58 -0500

    Added basic test.

--- src/lib/pattern.rs
+++ src/lib/pattern.rs
@@ -5,7 +5,7 @@ const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const UPPERS: [char; 26] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const LOWERS: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct RegexList {
     list: Vec<Regex>
 }
@@ -16,19 +16,19 @@ impl RegexList {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Regex {
     pattern: Pattern,
-    modifier: Modifier // ex '*', '?', "{2, 5}"
+    modifier: Option<Modifier> // ex '*', '?', "{2, 5}"
 }
 
 impl Regex {
     pub fn len(&self) -> usize {
-        self.pattern.len() + self.modifier.len()
+        &self.pattern.len() + &self.modifier.as_ref().map(|x| x.len()).unwrap_or(0)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum Pattern {
     Literal(String)
 }
@@ -41,16 +41,14 @@ impl Pattern {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum Modifier {
-    None
+
 }
 
 impl Modifier {
-    pub fn len(&self) -> usize {
-        match &self {
-            Modifier::None => 0
-        }
+    fn len(&self) -> usize {
+        0
     }
 }
 
@@ -98,10 +96,52 @@ impl TryFrom<&str> for Regex {
             (Some(pattern), None) => {
                 Ok(Regex {
                     pattern,
-                    modifier: Modifier::None
+                    modifier: None
                 })
             }
             (None, _) => Err(Self::Error::Empty),
         }
+    }
+}
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        let input = "test";
+        let result = RegexList::try_from(input).unwrap();
+        let correct =  RegexList {
+            list: vec![
+                Regex {
+                    pattern: Pattern::Literal("t".to_string()),
+                    modifier: None,
+                },
+                Regex {
+                    pattern: Pattern::Literal(
+                        "e".to_string()
+                    ),
+                    modifier: None,
+                },
+                Regex {
+                    pattern: Pattern::Literal(
+                        "s".to_string(),
+                    ),
+                    modifier: None,
+                },
+                Regex {
+                    pattern: Pattern::Literal(
+                        "t".to_string(),
+                    ),
+                    modifier: None,
+                },
+            ],
+        };
+        assert_eq!(result, correct);
     }
 }
