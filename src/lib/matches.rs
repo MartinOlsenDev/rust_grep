416f1d48860fd5ac4a1402e90c896588c0045bcc
Martin Olsen <Martin.Olsen2@protonmail.com>
2024-06-12 14:29:49 -0500

    Small bug fix.

--- src/lib/matches.rs
+++ src/lib/matches.rs
@@ -1,3 +1,3 @@
-pub trait Matches {
+pub trait Matches<T> {
     fn matches(&self, other: T) -> bool;
 }
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,4 +1,4 @@
 pub mod input;
 pub mod cli;
 pub mod regex;
-mod matches;
+pub mod matches;
